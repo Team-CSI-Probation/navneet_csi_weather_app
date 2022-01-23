57b214812a5ab676b946d9e3a49dcdbc2bd269b4
Navneet <agrawalnav6543@gmail.com>
2022-01-23 10:18:37 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -5,28 +5,29 @@ const api={
     base:"https://api.openweathermap.org/data/2.5/"
 }
 function App() {
- const apiKey ='4c7df8f2525e02705cac9f74af49328c'   
+    const[query,setQuery]=useState('');
   const[weather,setWeather]= useState({});
-  const[query,setQuery]=useState('');
+  
   
   const search = evt=>{
-      if(evt.key==="Enter"){
+      if(evt.key === "Enter"){
           fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
           .then(res => res.json())
           .then(result => {setWeather(result);
-        setQuery();
+        setQuery('');
      });
       }
   }
   return(
-      <div className ={
-          (typeof weather.main != "undefined")?(
-        (weather.weather[0].main==="sunny" ? 'container sunny':
-         weather.weather[0].main==="clear" ? 'container clear':
-         weather.weather[0].main==="haze" ? 'container haze':
-         weather.weather[0].main==="mist" ? 'container mist':
-         weather.weather[0].main==="clouds" ? 'container clouds':'container'))
-          : "container"
+    
+           
+           <div className={(typeof weather.main !="undefined")?(
+            (weather.weather[0].main==="Sunny"? 'app sunny':
+            weather.weather[0].main==="Clouds" ? 'app cloudy':
+            weather.weather[0].main==="Haze" ? 'app haze':
+            weather.weather[0].main==="Clear" ? 'app clear':
+            weather.weather[0].main==="Mist" ? 'app mist':'app'))
+            :'app'
            }>
 
       <main>
@@ -41,18 +42,18 @@ function App() {
            />
      </div>
      {(typeof weather.main!='undefined') ? (
-         <div>
-     <div className="location_box"> 
-     <div className="location">{weather.name},{weather.sys.country}</div>
+     <div>
+         <div className="location_box"> 
+           <div className="location">{weather.name},{weather.sys.country}</div>
 
-     </div>
-     <div className="weather_box">
-      <div className="temp">
-          {weather.main.temp}°C
+           </div>
+          <div className="weather_box">
+           <div className="temp">
+          {Math.round(weather.main.temp)}°C
                 </div>
-      <div className="weather">{weather.weather[0].main}</div>
+            <div className="weather">{weather.weather[0].main}</div>
 
-     </div>
+         </div>
      </div>
      ) : ('')}
       </main>
